183f7df402f437e4851f58cc80e960f98fc1fdaf
Anton Veldanov <anton.veldanov@gmail.com>
2019-09-06 23:08:02 -0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const data = [
+  { width: 600, height: 100, fill: 'yellow' }
+];
+
+
+
+
+
 const svg = d3.select('svg');
 
-svg.select('rect')
-  .attr('width', 200)
-  .attr('height', 100)
-  .attr('fill', 'pink')
+const rect = svg.select('rect')
+  .data(data)
+  .attr('width', (d, i, n) => {
+    console.log(i);
+    console.log(n);
+    return d.width
+  })
+  .attr('height', function (d) { return d.height; })
+  .attr('fill', function (d) { return d.fill; });
 
 
+console.log(rect);
