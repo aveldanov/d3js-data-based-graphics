57ec6ac967bf89755c9e0a6bd60d842307bec20f
Anton Veldanov <anton.veldanov@gmail.com>
2019-09-11 00:29:08 -0700

    d3.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+const name = document.querySelector('#name'); const cost = document.querySelector('#cost');
+const error = document.querySelector('#error');
+
+
+form.addEventListener('submit', (e) => {
+
+});
+
